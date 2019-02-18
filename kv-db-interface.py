48dcefda91960cd540dbf55b653f8135e390e9d3
Mike M <mikey.x88@gmail.com>
2019-02-17 21:24:31 -0500

    work on the python script

--- kv-db-interface.py
+++ kv-db-interface.py
@@ -3,6 +3,7 @@ import argparse
 from sqlalchemy import *
 from sqlalchemy.orm import *
 import json
+import re
 from pprint import pprint
 
 Base = declarative_base()
@@ -16,25 +17,78 @@ class KeyValues(Base):
     value = Column(BLOB, nullable=False)
 
 
-def main():
-    json_data = open('settings.json').read()
+def get_db_connection_string_from_settings_file(filename="settings.json"):
+    json_data = open(filename).read()
     settings = json.loads(json_data)
-    pprint(settings)
+    # pprint(settings)
 
-    sql_engine = settings['databaseEngine'] if 'databaseEngine' in json_data else 'sqlite'
+    # dialect+driver://username:password@host:port/database
+    db_dialect = settings['databaseEngine'] if 'databaseEngine' in json_data else 'sqlite'
+    db_driver = "+" + settings['driver'] if 'driver' in json_data else ''
     db_name = settings['databaseName'] if 'databaseName' in json_data else 'kv_db'
-    hostname = settings['hostname'] if 'hostname' in json_data else ''
+    db_username = settings['username'] if 'username' in json_data else ''
+    db_password = settings['password'] if 'password' in json_data and len(db_username) > 0 else ''
+    db_credentials = ""
 
-    if hostname == 'sqlite':
+    if len(db_username) > 0:
+        db_credentials += db_username
+        if len(db_password) > 0:
+            db_credentials += ":" + db_password
+        db_credentials += "@"
+    hostname = settings['hostname'] if 'hostname' in json_data else 'localhost'
+
+    port = settings['port'] if 'port' in json_data else None
+
+    if port is not None and (port > 0):
+        port = ":" + re.sub('[^0-9]', '', str(port))
+    else:
+        port = ''
+
+    if db_dialect == 'sqlite':
+        port = ''
         hostname = ''
+        db_driver = ''
+        db_credentials = ''
 
-    db_engine = create_engine('%s://%s/%s.db' % (sql_engine, hostname, db_name))
+    return '%s%s://%s%s%s/%s.db' % (db_dialect, db_driver, db_credentials, hostname, port, db_name)
+
+
+def get_all(session):
+    return session.query(KeyValues).all()
+
+
+def get(session, key):
+    raise NotImplemented
+
+
+def get_multiple(session, keys):
+    if type(keys) is not list:
+        raise TypeError("A list of keys is expected. Got %s instead." % str(type(keys)))
+    raise NotImplemented
+
+
+def insert(session, key, value):
+    raise NotImplemented
+
+
+def insert(session, key_values):
+    raise NotImplemented
+
+
+def update(session, key):
+    raise NotImplemented
+
+
+def main():
+    conn_string = get_db_connection_string_from_settings_file()
+    print("Connecting to: %s" % conn_string)
+
+    db_engine = create_engine(conn_string)
     Base.metadata.create_all(db_engine)
     Base.metadata.bind = db_engine
 
-    dbSession = sessionmaker(bind=db_engine)
-    session = dbSession()
-    
+    session = sessionmaker(bind=db_engine)()
+
     return
 
 
