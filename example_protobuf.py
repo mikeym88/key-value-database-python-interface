0914f457d8f05fe76f51a98db545aeaad82f5f77
Mike M <mikey.x88@gmail.com>
2019-02-19 22:00:34 -0500

    enhanced protobuf example

--- example_protobuf.py
+++ example_protobuf.py
@@ -4,12 +4,28 @@ import annoucement_pb2 as announcement_message
 
 def main():
     kv_db = KeyValueDatabaseInterface(connection_string="sqlite:///proto_buf.db")
-    announcement = announcement_message.Annoucement()
-    announcement.sender = "Mikey"
-    announcement.recipients.extend(['Joey', 'Sammy'])
-    announcement.message = "S.O.S."
-    kv_db.insert("message1", announcement.SerializeToString())
-    print("Key-Value Inserted: { '%s' : '%s' }" % (kv_db.get("message1").key, kv_db.get("message1").value))
+    message_to_serialize = announcement_message.Annoucement()
+    message_to_serialize.sender = "Mikey"
+    message_to_serialize.recipients.extend(['Joey', 'Sammy'])
+    message_to_serialize.message = "S.O.S."
+    print("The following the printed Protbuf object:")
+    print(message_to_serialize)
+
+    print("This is how it showed up serialized:")
+    print(message_to_serialize.SerializeToString())
+
+    print("Inserting the message...")
+    kv_db.insert("message1", message_to_serialize)
+    print("Retrieving the message...")
+    serialized_message_from_db = kv_db.get("message1").value
+    print("This is how it looks like in after it is retrieve from the database:")
+    print(serialized_message_from_db)
+
+    print("Deserializing...")
+    deserialized_object = announcement_message.Annoucement()
+    deserialized_object.ParseFromString(serialized_message_from_db)
+    print("Done. This is the deserialized message from the database:")
+    print(deserialized_object)
 
 
 if __name__ == "__main__":
